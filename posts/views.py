951bd9a6268736d77380b3a4ca258da67e3619b2
Виктория Аксентий <peta-holding@yandex.ru>
2021-04-02 21:27:58 +0300

    111

--- posts/views.py
+++ posts/views.py
@@ -3,10 +3,10 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models.query_utils import Q
 from django.shortcuts import get_object_or_404, redirect, render
+from users.forms import ProfileEditForm, UserEditForm
 
 from .forms import CommentForm, PostForm
 from .models import Follow, Group, Post, Profile
-from users.forms import UserEditForm, ProfileEditForm
 
 User = get_user_model()
 
@@ -45,6 +45,7 @@ def group_posts(request, slug):
             'group': group,
             'page': page,
             'is_active': True})
+            
 
 
 @login_required
