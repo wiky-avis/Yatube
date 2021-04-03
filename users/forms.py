a926baabb5b2e5d38c743953ad21f56c1ddf0151
wiky-avis <peta-holding@yandex.ru>
2021-04-03 14:12:13 +0300

    миниатюры картинок в форме.пока не удалось

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,19 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+# from string import Template
+# from django.utils.safestring import mark_safe
+# from django.conf import settings
 
 from posts.models import Profile
 
 User = get_user_model()
+
+
+# class PictureWidget(forms.widgets.Widget):
+#     def render(self, name, value, attrs=None, renderer=None):
+#         html = Template("""<img src="$media$link" width="50" height="60" />""")
+#         return mark_safe(html.substitute(media=settings.MEDIA_URL, link=value))
 
 
 class CreationForm(UserCreationForm):
@@ -23,3 +32,6 @@ class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('photo',)
+        # widgets = {
+        #     'photo': PictureWidget
+        # }
