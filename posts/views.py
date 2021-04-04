6726c0f3dc3bacdd43b03b84dd8140b41e41ec31
Виктория Аксентий <peta-holding@yandex.ru>
2021-04-04 09:09:58 +0300

    превью картинок в форме

--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,11 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, get_user_model, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.postgres.search import SearchVector
 from django.core.paginator import Paginator
 from django.db.models.query_utils import Q
-from django.shortcuts import get_object_or_404, redirect, render
-<<<<<<< HEAD
-=======
-from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-
->>>>>>> f462d4736c4dc8f90c3d45acfad3139064468f2a
 from users.forms import ProfileEditForm, UserEditForm
 
 from .forms import CommentForm, PostForm
--- users/forms.py
+++ users/forms.py
@@ -1,19 +1,25 @@
+from string import Template
+
 from django import forms
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from string import Template
 from django.utils.safestring import mark_safe
-from django.conf import settings
-
 from posts.models import Profile
 
 User = get_user_model()
 
 
-class PictureWidget(forms.widgets.Widget):
-    def render(self, name, value, attrs=None, renderer=None):
-        html = Template("""<img src="$media$link" width="50" height="60" />""")
-        return mark_safe(html.substitute(media=settings.MEDIA_URL, link=value))
+class PictureWidget(forms.widgets.FileInput):
+    """Виджет для отображения превью картинки в форме"""
+    def render(self, name, value, attrs=None, renderer=None, **kwargs):
+        input_html = super().render(name, value, attrs=None, **kwargs)
+        html = Template(
+            """<img src="$media$link" width="80" height="80" />""")
+        img_html = mark_safe(
+            html.substitute(media=settings.MEDIA_URL, link=value))
+        content = mark_safe(f'{input_html}<br><br>{img_html}')
+        return content
 
 
 class CreationForm(UserCreationForm):
@@ -32,6 +38,6 @@ class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('photo',)
-        # widgets = {
-        #     'photo': PictureWidget
-        # }
+        widgets = {
+            'photo': PictureWidget
+        }
